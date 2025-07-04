5d2866c05342e7874c5704a18037f57b55c88a13
Zaidsaadedin1 <zaidsaadedin11@gmail.com>
2025-07-05 00:45:01 +0300

    oimg fixed it again

--- app/Apis/types/axios.ts
+++ app/Apis/types/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie"; // Use js-cookie for client-side cookies
 
 const baseURL =
-  "https://dr-majdoline-aldeek-apis-production.up.railway.app//api";
+  "https://dr-majdoline-aldeek-apis-production.up.railway.app/api";
 //const baseURL = "http://localhost:5209/api";
 
 const axiosInstance = axios.create({
